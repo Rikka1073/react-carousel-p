98fcbe02a70c115b8e514eba718aa3275d1a966f
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-12-01 11:54:17 +0900

    add:カルーセル完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import useEmblaCarousel from "embla-carousel-react";
 import "./App.css";
+import { useEffect } from "react";
+import Autoplay from "embla-carousel-autoplay";
 
 function App() {
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
   return (
     <>
       <h1>Vite + React</h1>
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          <div className="embla__slide">Slide 1</div>
+          <div className="embla__slide">Slide 2</div>
+          <div className="embla__slide">Slide 3</div>
+        </div>
+      </div>
     </>
   );
 }
